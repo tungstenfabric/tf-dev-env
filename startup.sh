befe7f3a7a1da53be1adbe76cd52aa1ac9d53e12
Andrey Pavlov <andrey.mp@gmail.com>
2019-04-30 13:19:05 +0300

    make yum install quiet

--- startup.sh
+++ startup.sh
@@ -59,7 +59,7 @@ function is_up () {
 function install_docker() {
   (yum install -y yum-utils device-mapper-persistent-data lvm2 \
   && yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo \
-  && yum install docker-ce docker-ce-cli containerd.io) \
+  && yum install -y docker-ce docker-ce-cli containerd.io) \
   || (echo Failed to install docker with error $? && exit 1)
 }
 
