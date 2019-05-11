4e9919d7391eeaf50c07aa2719910b2fe9a9be7b
Andrey Pavlov <andrey.mp@gmail.com>
2019-05-11 13:26:35 +0300

    fix preparation for test container

--- scripts/prepare-test-containers.sh
+++ scripts/prepare-test-containers.sh
@@ -4,5 +4,4 @@ REPODIR=/root/src/${CANONICAL_HOSTNAME}/Juniper/contrail-test
 BRANCH=${SB_BRANCH:-master}
 
 [ -d ${REPODIR} ] || git clone https://github.com/Juniper/contrail-test -b ${BRANCH}  ${REPODIR}
-cp common.env ${REPODIR}
-cp tpc.repo.template ${REPODIR}/docker/test/tpc.repo
+cp tpc.repo.template ${REPODIR}/docker/base/tpc.repo
