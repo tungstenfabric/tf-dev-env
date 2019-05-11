c0a776b78497c042f99e5ff284b0379d12242f3e
Andrey Pavlov <andrey.mp@gmail.com>
2019-05-11 14:19:54 +0300

    revert common.env copying for test container

--- scripts/prepare-test-containers.sh
+++ scripts/prepare-test-containers.sh
@@ -4,4 +4,5 @@ REPODIR=/root/src/${CANONICAL_HOSTNAME}/Juniper/contrail-test
 BRANCH=${SB_BRANCH:-master}
 
 [ -d ${REPODIR} ] || git clone https://github.com/Juniper/contrail-test -b ${BRANCH}  ${REPODIR}
+cp common.env ${REPODIR}
 cp tpc.repo.template ${REPODIR}/docker/base/tpc.repo
