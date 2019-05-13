8c98a2d0a39778b573ee649442e496cdbf81770c
Alexey Osorgin <alexey.osorgin@gmail.com>
2019-05-13 14:47:35 +0300

    Do not copy config/etc/yum.repos.d/tpc.repo on startup

--- startup.sh
+++ startup.sh
@@ -181,7 +181,6 @@ rpm_repo_ip=$(docker inspect --format '{{ .NetworkSettings.Gateway }}' contrail-
 sed -e "s/rpm-repo/${rpm_repo_ip}/g" -e "s/registry/${registry_ip}/g" -e "s/6666/${REGISTRY_PORT}/g" common.env.tmpl > common.env
 sed -e "s/rpm-repo/${rpm_repo_ip}/g" -e "s/contrail-registry/${registry_ip}/g" -e "s/6666/${REGISTRY_PORT}/g" vars.yaml.tmpl > vars.yaml
 sed -e "s/rpm-repo/${rpm_repo_ip}/g" -e "s/registry/${registry_ip}/g" dev_config.yaml.tmpl > dev_config.yaml
-cp -f tpc.repo.template config/etc/yum.repos.d/tpc.repo
 
 if [[ "$own_vm" -eq 0 ]]; then
   if ! is_created "contrail-developer-sandbox"; then
