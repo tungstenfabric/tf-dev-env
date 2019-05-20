eaba9f543261267424449ff28ada39f5b2e84c07
Alexey Osorgin <alexey.osorgin@gmail.com>
2019-05-20 19:00:31 +0300

    Parallel builds of containers\deployers and test containers

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -15,10 +15,33 @@ if [[ "${AUTOBUILD}" -eq 1 ]]; then
     fi
 
     make setup dep fetch_packages
-    make rpm containers deployers
+    make rpm
 
     if [[ "${BUILD_TEST_CONTAINERS}" == "1" ]]; then
-        make test-containers
+        make containers deployers | sed "s/^/containers: /" &
+        containers_pid=$!
+
+        make test-containers | sed "s/^/test_containers: /" &
+        test_containers_pid=$!
+
+        wait $containers_pid
+        containers_status=$?
+
+        wait $test_containers_pid
+        test_containers_status=$?
+
+        echo Build of containers with deployers has finished with status $containers_status
+        echo Build of test containers has finished with status $test_containers_status
+
+        if [[ "$containers_status" != "0" ]]; then
+            exit $containers_status
+        fi
+
+        if [[ "$test_containers_status" != "0" ]]; then
+            exit $test_containers_status
+        fi
+    else
+        make containers deployers
     fi
 
     exit 0
