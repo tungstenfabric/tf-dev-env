1c36af5f690e43c887423adb8ac203b7b7f95f31
Alexey Osorgin <alexey.osorgin@gmail.com>
2019-06-03 19:03:48 +0300

    Revert "Link contrail-helm-deployer, openstack-helm and openstack-helm-infra project form external repositories"
    This reverts commit 379af68751d67cc88164c5d40fbc5e065ae4a656.

--- scripts/prepare-deployers.sh
+++ scripts/prepare-deployers.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-PROJECTDIR=/root/src/${CANONICAL_HOSTNAME}/Juniper
-REPODIR=${PROJECTDIR}/contrail-deployers-containers
+REPODIR=/root/src/${CANONICAL_HOSTNAME}/Juniper/contrail-deployers-containers
 BRANCH=${SB_BRANCH:-master}
-
-function link_deployers_project() {
-    project_name=$1
-
-    if [ -d ${PROJECTDIR}/${project_name} ] && [ ! -d /root/${project_name} ]; then
-        ln -s ${PROJECTDIR}/${project_name} /root/${project_name}
-    fi
-}
 
 [ -d ${REPODIR} ] || git clone https://github.com/Juniper/contrail-deployers-containers -b ${BRANCH}  ${REPODIR}
 for file in tpc.repo.template common.env ; do
@@ -18,7 +9,3 @@ for file in tpc.repo.template common.env ; do
     cp $file ${REPODIR}
   fi
 done
-
-link_deployers_project contrail-helm-deployer
-link_deployers_project openstack-helm
-link_deployers_project openstack-helm-infra
