50bea590e4919ef59fba673259217d2ba7e48365
Andrey Pavlov <andrey.mp@gmail.com>
2019-06-25 13:13:47 +0300

    write datetime to stdout for each build step

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -11,12 +11,22 @@ if [[ "${AUTOBUILD}" -eq 1 ]]; then
     cd $CONTRAIL_DEV_ENV
 
     if [[ "${SRC_MOUNTED}" != "1" ]]; then
+        echo "INFO: make sync  $(date)"
         make sync
     fi
 
-    make setup dep fetch_packages
-    make rpm create-repo
+    echo "INFO: make setup  $(date)"
+    make setup
+    echo "INFO: make dep  $(date)"
+    make dep
+    echo "INFO: make fetch_packages  $(date)"
+    make fetch_packages
+    echo "INFO: make rpm  $(date)"
+    make rpm
+    echo "INFO: make create-repo  $(date)"
+    make create-repo
 
+    echo "INFO: make containers  $(date)"
     if [[ "${BUILD_TEST_CONTAINERS}" == "1" ]]; then
         make prepare-containers containers-only prepare-deployers deployers-only | sed "s/^/containers: /" &
         containers_pid=$!
@@ -31,6 +41,7 @@ if [[ "${AUTOBUILD}" -eq 1 ]]; then
         if [[ "$test_containers_status" != "0" ]]; then
             echo Termination build containers job
             kill $containers_pid
+            echo "INFO: make test containers failed  $(date)"
             exit $test_containers_status
         fi
 
@@ -40,6 +51,7 @@ if [[ "${AUTOBUILD}" -eq 1 ]]; then
         echo Build of containers with deployers has finished with status $containers_status
 
         if [[ "$containers_status" != "0" ]]; then
+            echo "INFO: make containers failed  $(date)"
             exit $containers_status
         fi
     else
@@ -47,6 +59,7 @@ if [[ "${AUTOBUILD}" -eq 1 ]]; then
         make prepare-deployers deployers-only
     fi
 
+    echo "INFO: make successful  $(date)"
     exit 0
 fi
 
