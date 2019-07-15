2ff50d18239e8e4f3a9a90c7f89259273c77f09d
Andrey Pavlov <andrey.mp@gmail.com>
2019-07-15 15:10:53 +0300

    add utf8 locale for build stage

--- container/build.sh
+++ container/build.sh
@@ -17,4 +17,4 @@ shift $((OPTIND-1))
 TAG=${1:-latest}
 echo Building tf-dev-env image: ${IMAGE}:${TAG}
 cp ../tpc.repo.template tpc.repo
-docker build --build-arg BRANCH=${BRANCH} --no-cache --tag ${IMAGE}:${TAG} .
+docker build --build-arg LC_ALL=en_US.UTF-8 --build-arg LANG=en_US.UTF-8 --build-arg LANGUAGE=en_US.UTF-8 --build-arg BRANCH=${BRANCH} --no-cache --tag ${IMAGE}:${TAG} .
