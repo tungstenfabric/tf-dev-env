04298df19a283d02e5d9e8fbbee0f3aab9949aa0
alexey-mr <alexey.morlang@gmail.com>
2019-07-17 15:32:00 +0300

    Skip build output if flag set for devenv and test containers
    - save output to log file only if flag set

--- scripts/build-test-containers.sh
+++ scripts/build-test-containers.sh
@@ -15,15 +15,32 @@ fi
 CONTRAIL_VERSION=${CONTRAIL_VERSION:-"dev"}
 CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"${CONTRAIL_VERSION}"}
 openstack_versions=${OPENSTACK_VERSIONS:-"ocata,queens,rocky"}
+CONTRAIL_KEEP_LOG_FILES=${CONTRAIL_KEEP_LOG_FILES:-'false'}
 
 pushd ${REPODIR}
 
-echo Build base test container
-if ! ./build-container.sh base \
-        --registry-server ${CONTRAIL_REGISTRY} \
-        --tag ${CONTRAIL_CONTAINER_TAG} ; then
+function append_log() {
+  local logfile=$1
+  while read line ; do
+    if [[ "${CONTRAIL_KEEP_LOG_FILES,,}" != 'true' ]] ; then
+      echo "$line" | tee -a $logfile
+    else
+      echo "$line" >> $logfile
+    fi
+  done
+}
+
+logfile="./build-test-base.log"
+echo Build base test container | tee $logfile
+./build-container.sh base \
+  --registry-server ${CONTRAIL_REGISTRY} \
+  --tag ${CONTRAIL_CONTAINER_TAG} 2>&1 | append_log $logfile
+if [ ${PIPESTATUS[0]} -eq 0 ]; then
+  echo Build base test container finished successfully | tee -a $logfile
+  [[ "${CONTRAIL_KEEP_LOG_FILES,,}" != 'true' ]] && rm -f $logfile
+else
   popd
-  echo Failed to build base test container
+  echo Failed to build base test container | append_log $logfile
   exit 1
 fi
 
@@ -35,7 +52,7 @@ for openstack_version in ${openstack_versions//,/ } ; do
         openstack_repo_option="--openstack-repo ${OPENSTACK_REPOSITORY}/openstack-${openstack_version}"
     fi
 
-    echo Start build test container for ${openstack_version}
+    echo Start build test container for ${openstack_version} | tee "./build-test-${openstack_version}.log"
     ./build-container.sh test \
         --base-tag ${CONTRAIL_CONTAINER_TAG} \
         --tag ${openstack_version}-${CONTRAIL_CONTAINER_TAG} \
@@ -43,7 +60,7 @@ for openstack_version in ${openstack_versions//,/ } ; do
         --sku ${openstack_version} \
         --contrail-repo ${CONTRAIL_REPOSITORY} \
         ${openstack_repo_option} \
-        --post &
+        --post | append_log "./build-test-${openstack_version}.log" &
     jobs+=( [$openstack_version]=$! )
 done
 
@@ -54,8 +71,11 @@ for openstack_version in ${openstack_versions//,/ } ; do
     kill %${jobs[$openstack_version]}
   fi
   if ! wait ${jobs[$openstack_version]} ; then
-    echo "ERROR: Faild to build test container for ${openstack_version}"
+    echo "ERROR: Faild to build test container for ${openstack_version}" | tee -a "./build-test-${openstack_version}.log"
     res=1
+  else
+    echo Build test container for ${openstack_version} finished successfully | tee -a "./build-test-${openstack_version}.log"
+    [[ "${CONTRAIL_KEEP_LOG_FILES,,}" != 'true' ]] && rm -f "./build-test-${openstack_version}.log"
   fi
 done
 
