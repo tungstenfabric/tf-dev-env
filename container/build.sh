63ac377f1581cb38f992c56dc05716855bf5d83b
alexey-mr <alexey.morlang@gmail.com>
2019-07-17 14:32:19 +0300

    Added log file for tf-dev-env build

--- container/build.sh
+++ container/build.sh
@@ -15,6 +15,23 @@ done
 shift $((OPTIND-1))
 
 TAG=${1:-latest}
-echo Building tf-dev-env image: ${IMAGE}:${TAG}
+CONTRAIL_KEEP_LOG_FILES=${CONTRAIL_KEEP_LOG_FILES:-'false'}
+
+logfile="./build-tf-dev-env.log"
+echo Building tf-dev-env image: ${IMAGE}:${TAG} | tee $logfile
 cp ../tpc.repo.template tpc.repo
-docker build --build-arg LC_ALL=en_US.UTF-8 --build-arg LANG=en_US.UTF-8 --build-arg LANGUAGE=en_US.UTF-8 --build-arg BRANCH=${BRANCH} --no-cache --tag ${IMAGE}:${TAG} .
+
+build_opts="--build-arg LC_ALL=en_US.UTF-8 --build-arg LANG=en_US.UTF-8 --build-arg LANGUAGE=en_US.UTF-8 --build-arg BRANCH=${BRANCH} --no-cache --tag ${IMAGE}:${TAG} ."
+
+if [[ "${CONTRAIL_KEEP_LOG_FILES,,}" != 'true' ]] ; then
+   docker build $build_opts 2>&1 | tee -a $logfile
+   result=${PIPESTATUS[0]}
+   if [ $result -eq 0 ]; then
+      rm -f $logfile
+   fi
+else
+   # skip output into terminal
+   docker build $build_opts >> $logfile 2>&1
+fi
+
+exit $result
