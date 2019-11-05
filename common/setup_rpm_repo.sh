3c98365f27b9c6ae45ca7285052802d0869b266f
alexey-mr <alexey.morlang@gmail.com>
2019-11-05 23:57:49 +0300

    Fixed rpm repo port

--- common/setup_rpm_repo.sh
+++ common/setup_rpm_repo.sh
@@ -32,7 +32,7 @@ ensure_port_free 80
 
 if ! is_container_created "$RPM_CONTAINER_NAME"; then
   docker run -t --name "$RPM_CONTAINER_NAME" \
-    -d -p 6667:80 \
+    -d -p ${RPM_REPO_PORT}:80 \
     -v ${rpm_source}:/var/www/localhost/htdocs \
     m4rcu5/lighttpd >/dev/null
   echo "$RPM_CONTAINER_NAME created"
