ffa5935ef0dec5b4c0707ad5bbf1e7ac352b4cc1
Prabhjot Singh Sethi <prabhjot@atsgen.com>
2019-11-07 15:11:12 +0530

    mark autobuild only if configured
    set option AUTOBUILD only if corresponding env is set
    
    Signed-off-by: Prabhjot Singh Sethi <prabhjot@atsgen.com>

--- startup.sh
+++ startup.sh
@@ -65,7 +65,9 @@ if ! is_container_created "$TF_DEVENV_CONTAINER_NAME"; then
     options+=" -e SITE_MIRROR=${SITE_MIRROR}"
   fi
 
-  options+=" -e AUTOBUILD=1"
+  if [[ "${AUTOBUILD}" == "1" ]]; then
+    options+=" -e AUTOBUILD=1"
+  fi
 
   if [[ "$BUILD_DEV_ENV" != '1' ]] && ! is_container_created $DEVENV_IMAGE ; then
     if ! docker inspect $DEVENV_IMAGE >/dev/null 2>&1 && ! docker pull $DEVENV_IMAGE ; then
