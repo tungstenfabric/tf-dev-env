4468cee8e232f2c523903508b29a5f2c94a2cb06
Ivan Timofeev <ivanv.timofeev@gmail.com>
2019-11-20 16:32:10 +0300

    Rename startup.sh to run.sh

--- README.md
+++ README.md
@@ -66,14 +66,14 @@ git clone https://github.com/tungstenfabric/tf-dev-env
 
 ### 4. Execute startup script to start all required containers
 ```
-sudo -E ./tf-dev-env/startup.sh
+sudo -E ./tf-dev-env/run.sh
 ```
 
 **Note:** This command runs container `opencontrailnightly/developer-sandbox-centos:master` from [opencontrailnightly docker hub](https://hub.docker.com/r/opencontrailnightly/developer-sandbox/) by
 default. You can specify different image and/or tag using flags, e.g.
 
-1. to develop on nightly R5.0 container use: `sudo -E ./startup.sh -t R5.1`
-2. to develop code based on a tagged `r5.1` release, use: `sudo -E ./startup.sh -i opencontrail/developer-sandbox -t r5.1`
+1. to develop on nightly R5.0 container use: `sudo -E ./run.sh -t R5.1`
+2. to develop code based on a tagged `r5.1` release, use: `sudo -E ./run.sh -i opencontrail/developer-sandbox -t r5.1`
 Also you can export BUILD_DEV_ENV=1 to explicit build of sandbox container locally if sandbox is not run yet.
 Please note - if you don't pass '-t' option (or pass it as 'latest') then latest tag for sandbox image and master branch of contrail-vnc repo will be used. If you pass something else then this tag will be used for sandbox image and contrail-vnc will be cloned by the same branch name.
 
---
+++ container/run.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+[ -n "$DEBUG" ] && set -x
+
+set -eo pipefail
+
+stages=${1//,/ }
+
+declare -a all_stages=(fetch configure compile package)
+
+CANONICAL_HOSTNAME=${CANONICAL_HOSTNAME:-"review.opencontrail.org"}
+
+if [[ -d /config ]]; then
+  cp -rf /config/* /
+  export CONTRAIL_CONFIG_DIR=${CONTRAIL_CONFIG_DIR:-"/config"}
+fi
+
+cd $CONTRAIL_DEV_ENV
+STAGES_DIR="${CONTRAIL}/.stages"
+mkdir -p $STAGES_DIR
+
+function fetch() {
+    echo "INFO: make sync  $(date)"
+    make sync
+}
+
+function configure() {
+    echo "INFO: make setup  $(date)"
+    make setup
+
+    echo "INFO: make dep fetch_packages  $(date)"
+    # targets can use yum and will block each other. don't run them in parallel
+    make dep fetch_packages
+}
+
+function compile() {
+    echo "INFO: Check variables used by makefile"
+    uname -a
+    make info
+    echo "INFO: make rpm  $(date)"
+    make rpm
+    make create-repo
+}
+
+function package() {
+    echo "INFO: Check variables used by makefile"
+    uname -a
+    make info
+    echo "INFO: make containers  $(date)"
+    # prepare rpm repo and repos
+    echo "INFO: make create-repo prepare-containers prepare-deployers prepare-test-containers  $(date)"
+    make -j 3 prepare-containers prepare-deployers prepare-test-containers
+    build_status=$?
+    if [[ "$build_status" != "0" ]]; then
+        echo "INFO: make prepare containers failed with code $build_status  $(date)"
+        exit $build_status
+    fi
+
+    # prebuild general base as it might be used by deployers
+    echo "INFO: make container-general-base  $(date)"
+    make container-general-base
+    build_status=$?
+    if [[ "$build_status" != "0" ]]; then
+        echo "INFO: make general-base container failed with code $build_status $(date)"
+        exit $build_status
+    fi
+
+    # build containers
+    echo "INFO: make containers-only deployers-only test-containers-only  $(date)"
+    make -j 6 containers-only deployers-only test-containers-only
+    build_status=$?
+    if [[ "$build_status" != "0" ]]; then
+        echo "INFO: make containers failed with code $build_status $(date)"
+        exit $build_status
+    fi
+
+    echo Build of containers with deployers has finished successfully
+}
+
+function run_stage() {
+    $1
+    touch $STAGES_DIR/$1
+}
+
+function finished_stage() {
+    [ -e $STAGES_DIR/$1 ]
+}
+
+function cleanup() {
+    local stage=${1:-'*'}
+    rm -f $STAGES_DIR/$stage
+}
+
+function enabled() {
+    [[ "$1" =~ "$2" ]]
+}
+
+# select default stages
+if [[ -z "$stages" ]] ; then 
+    if ! finished_stage 'fetch' ; then 
+        run_stage fetch
+    fi
+    if ! finished_stage 'configure' ; then
+        run_stage configure
+    fi
+elif [[ "$stages" =~ 'build' ]] ; then 
+    # run default stages for 'build' option
+    for stage in ${all_stages[@]} ; do
+        if ! finished_stage "$stage" ; then 
+            run_stage $stage
+        fi
+    done
+else
+    # run selected stages
+    for stage in ${stages} ; do
+        if [[ "$stages" =~ $stage ]] ; then
+            run_stage $stage
+        fi
+    done
+fi
+
+
+echo "INFO: make successful  $(date)"
