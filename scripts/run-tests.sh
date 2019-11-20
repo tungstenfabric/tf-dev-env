a69b2ec8fe6d78c5b6f71a04e998e899ff890cdb
OlegBravo <oleg.bravo.ru@gmail.com>
2019-11-20 18:42:46 +0300

    fix unit test support

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,16 +3,14 @@
 TARGET=${1:-}
 JOBS=${JOBS:-1}
 
+
+scriptdir=$(realpath $(dirname "$0"))
 cd /root/contrail/
-if [[ $TARGET == *":test" ]]; then
-  echo "INFO: Starting unit tests for provided target: " $TARGET
-  python3 ../tf-dev-env/scripts/run-tests.py -j $JOBS $TARGET
-else
-  echo "INFO: Starting unit tests for package: " $TARGET
-  for test in $( jq -r .default.scons_test_targets[] controller/ci_unittests.json) ; do
-    if [[ $test == *"$TARGET"* ]] ; then
-      python3 ../tf-dev-env/scripts/run-tests.py -j $JOBS $test
-    fi;
-  done;
-fi
+for test in $(jq -r ".[].scons_test_targets[]"  controller/ci_unittests.json| sort | uniq) ; do
+  if [ ! -z "$TARGET" ] && [[ $test != *"$TARGET"* ]]
+    then continue
+  fi
+echo "INFO: Starting unit tests for package " $test
+python3 scriptdir/run-tests.py -j $JOBS $test
+done;
 
