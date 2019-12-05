da33728c214e0ee13787dae322547ab65f8a24e7
Andrey Pavlov <andrey.mp@gmail.com>
2019-12-05 20:10:17 +0300

    run all UT even if one failed

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,20 +3,26 @@
 TARGET=${1:-}
 JOBS=${JOBS:-1}
 
-
 scriptdir=$(realpath $(dirname "$0"))
-cd /root/contrail/
 
-if [[ -f unittest_targets ]] ; then
+res=0
+if [[ -f /root/contrail/unittest_targets ]] ; then
   for utest in $(cat unittest_targets) ; do
-    $scriptdir/run-tests.py -j $JOBS $utest
+    echo "INFO: Starting unit tests for package $utest"
+    $scriptdir/run-tests.py -j $JOBS $utest || res=1
   done
 else
   for utest in $(jq -r ".[].scons_test_targets[]"  controller/ci_unittests.json| sort | uniq) ; do
-    if [ ! -z "$TARGET" ] && [[ $utest != *"$TARGET"* ]]
-      then continue
+    if [[ -n "$TARGET" && "$utest" != *"$TARGET"* ]]; then
+      continue
     fi
-    echo "INFO: Starting unit tests for package " $utest
-    $scriptdir/run-tests.py -j $JOBS $utest
+    echo "INFO: Starting unit tests for package $utest"
+    $scriptdir/run-tests.py -j $JOBS $utest || res=1
   done
 fi
+
+if [[ "$res" != '0' ]]; then
+  echo "ERROR: some UT failed"
+fi
+
+exit $res
