00fbc9ee3e8547b99d82d4084cb45888d7632a2b
OlegBravo <oleg.bravo.ru@gmail.com>
2019-12-05 16:57:52 +0300

    add unitttests support

--- scripts/fetch-packages.sh
+++ scripts/fetch-packages.sh
@@ -9,8 +9,5 @@ if ! output=`python3 -u fetch_packages.py $opts 2>&1` ; then
   echo "$output"
   exit 1
 fi
-if [[ -z "${GERRIT_PROJECT}"]] ; then
-  gather-unittest-targets.py > /root/tf-dev-env/ut_targets
-fi
 
 echo "$output" | grep -Ei 'Processing|patching'
--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,18 +7,16 @@ JOBS=${JOBS:-1}
 scriptdir=$(realpath $(dirname "$0"))
 cd /root/contrail/
 
-if [[-f /root/tf-dev-env/ut_targets ]]
+if [[ -f /root/tf-dev-env/ut_targets ]] ; then
   for utest in $(cat /root/tf-dev-env/ut_targets) ; do
     $scriptdir/run-tests.py -j $JOBS $utest
-  done;
+  done
+else
+  for utest in $(jq -r ".[].scons_test_targets[]"  controller/ci_unittests.json| sort | uniq) ; do
+    if [ ! -z "$TARGET" ] && [[ $utest != *"$TARGET"* ]]
+      then continue
+    fi
+    echo "INFO: Starting unit tests for package " $utest
+    $scriptdir/run-tests.py -j $JOBS $utest
+  done
 fi
-
-
-for utest in $(jq -r ".[].scons_test_targets[]"  controller/ci_unittests.json| sort | uniq) ; do
-  if [ ! -z "$TARGET" ] && [[ $utest != *"$TARGET"* ]]
-    then continue
-  fi
-  echo "INFO: Starting unit tests for package " $utest
-  $scriptdir/run-tests.py -j $JOBS $utest
-done;
-
