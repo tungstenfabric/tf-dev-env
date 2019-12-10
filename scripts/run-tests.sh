3cd42cb5e854349eac1ca009216aa4042963fe55
OlegBravo <oleg.bravo.ru@gmail.com>
2019-12-10 23:14:29 +0300

    store ut log directly to file

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,7 +5,7 @@ JOBS=${JOBS:-$(nproc)}
 
 scriptdir=$(realpath $(dirname "$0"))
 
-cd /root/contrail 
+cd /root/contrail
 res=0
 if [[ -f ./unittest_targets ]] ; then
   echo "INFO: targets to run:"
@@ -13,21 +13,25 @@ if [[ -f ./unittest_targets ]] ; then
   echo ; echo
   for utest in $(cat ./unittest_targets) ; do
     echo "INFO: Starting unit tests for target $utest"
-    if ! $scriptdir/run-tests.py -j $JOBS $utest ; then
+    logfilename=$( echo $utest |  cut -f1 -d ':' | rev | cut -f1 -d'/' | rev )
+    if ! $scriptdir/run-tests.py -j $JOBS $utest  &> /root/contrail/logs/$logfilename ; then
       res=1
       echo "ERROR: $utest failed"
     fi
+    echo "INFO: Unit test log is available at /root/contrail/logs/$logfilename"
   done
 else
   for utest in $(jq -r ".[].scons_test_targets[]"  controller/ci_unittests.json| sort | uniq) ; do
+    logfilename=$( echo $utest |  cut -f1 -d ':' | rev | cut -f1 -d'/' | rev )
     if [[ -n "$TARGET" && "$utest" != *"$TARGET"* ]]; then
       continue
     fi
     echo "INFO: Starting unit tests for target $utest"
-    if ! $scriptdir/run-tests.py -j $JOBS $utest ; then
+    if ! $scriptdir/run-tests.py -j $JOBS $utest &> /root/contrail/logs/$logfilename ; then
       res=1
       echo "ERROR: $utest failed"
     fi
+    echo "INFO: Unit test log is available at /root/contrail/logs/$logfilename"
   done
 fi
 
