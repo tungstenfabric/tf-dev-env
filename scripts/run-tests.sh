dfd662d966a46fb621c4653d684c60178e67c227
Andrey Pavlov <andrey-mp@yandex.ru>
2019-12-12 11:44:40 -0800

    add infra for ui/vcenter tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,39 +7,49 @@ scriptdir=$(realpath $(dirname "$0"))
 
 cd /root/contrail
 
-mkdir -p logs
+function controller_ut() {
+  local logs_path='/root/contrail/logs'
+  mkdir -p "$logs_path"
 
-res=0
-if [[ -f ./unittest_targets ]] ; then
+  local utest
+  local targets_file='/root/contrail/unittest_targets'
+  if [[ ! -f "$targets_file" ]] ; then
+    targets_file='/tmp/unittest_targets'
+    rm "$targets_file" && touch "$targets_file"
+    for utest in $(jq -r ".[].scons_test_targets[]"  controller/ci_unittests.json| sort | uniq) ; do
+      if [[ -z "$TARGET" || "$utest" == *"$TARGET"* ]]; then
+        echo "$utest" >> "$targets_file"
+      fi
+    done
+  fi
+
+  local res=0
   echo "INFO: targets to run:"
-  cat ./unittest_targets
+  cat "$targets_file"
   echo ; echo
-  for utest in $(cat ./unittest_targets) ; do
+  for utest in $(cat "$targets_file") ; do
     echo "INFO: Starting unit tests for target $utest"
-    logfilename=$( echo $utest |  cut -f1 -d ':' | rev | cut -f1 -d'/' | rev )
-    if ! $scriptdir/run-tests.py -j $JOBS $utest  &> /root/contrail/logs/$logfilename ; then
+    local logfilename=$(echo $utest | cut -f 1 -d ':' | rev | cut -f 1 -d '/' | rev)
+    if ! $scriptdir/run-tests.py -j $JOBS $utest  &> $logs_path/$logfilename ; then
       res=1
       echo "ERROR: $utest failed"
     fi
-    echo "INFO: Unit test log is available at /root/contrail/logs/$logfilename"
+    echo "INFO: Unit test log is available at $logs_path/$logfilename"
   done
-else
-  for utest in $(jq -r ".[].scons_test_targets[]"  controller/ci_unittests.json| sort | uniq) ; do
-    logfilename=$( echo $utest |  cut -f1 -d ':' | rev | cut -f1 -d'/' | rev )
-    if [[ -n "$TARGET" && "$utest" != *"$TARGET"* ]]; then
-      continue
-    fi
-    echo "INFO: Starting unit tests for target $utest"
-    if ! $scriptdir/run-tests.py -j $JOBS $utest &> /root/contrail/logs/$logfilename ; then
-      res=1
-      echo "ERROR: $utest failed"
-    fi
-    echo "INFO: Unit test log is available at /root/contrail/logs/$logfilename"
-  done
-fi
 
-if [[ "$res" != '0' ]]; then
-  echo "ERROR: some UT failed"
+  if [[ "$res" != '0' ]]; then
+    echo "ERROR: some UT failed"
+  fi
+  return $res
+}
+
+if [[ "$TARGET" == 'ui' ]]; then
+  echo "INFO: Running web ui tests"
+elif [[ "$TARGET" == 'vcenter' ]]; then
+  echo "INFO: Running vcenter tests"
+else
+  echo "INFO: Running controller tests"
+  controller_ut
 fi
 
 exit $res
