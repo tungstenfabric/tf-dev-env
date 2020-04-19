0bfbf681e36e015f7c8c600e90d71c6cd31b56ab
Alexandre Levine <alexandrelevine@gmail.com>
2020-04-19 12:04:11 +0300

    An attempt to remove standalone rpm container and integrate rpm repo into sandbox
    Change-Id: I5af61d31d6901dd45e38190bea7eadcbf8e8fa72

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# just wait
-tail -f /dev/null
+CONTRAIL_REPOSITORY=${CONTRAIL_REPOSITORY:-6667}
+
+sudo sed -i 's/Listen 80/Listen ${CONTRAIL_REPOSITORY}/' /etc/httpd/conf/httpd.conf
+sudo sed -i 's/\/var\/www\/html/\/var\/www\/html\/repo/' /etc/httpd/conf/httpd.conf
+sudo ln -s /centos/contrail/RPMS /var/www/html/repo
+sudo /usr/sbin/httpd -DNO_DETACH -DFOREGROUND
