45d25f819a1257f007e859ece8541fdb9c7976d1
Andrey Pavlov <andrey-mp@yandex.ru>
2020-05-04 00:41:23 +0300

    rework gathering UT to take TF migration into account
    Change-Id: I31356a270353535f4797529bd8b7ec1a0c870145

--- scripts/gather-unittest-targets.py
+++ scripts/gather-unittest-targets.py
@@ -3,43 +3,40 @@
 import json
 import sys
 import os
-
-
-def convert_path(path):
-    if path.startswith("contrail-sandesh/"):
-        path = path.replace("contrail-sandesh/", "tools/sandesh/")
-    elif path.startswith("contrail-generateDS/"):
-        path = path.replace("contrail-generateDS/", "tools/generateds/")
-    elif path.startswith("contrail-vrouter/"):
-        path = path.replace("contrail-vrouter/", "vrouter/")
-    elif path.startswith("contrail-common/"):
-        path = path.replace("contrail-common/", "src/contrail-common/")
-    elif path.startswith("contrail-analytics/"):
-        path = path.replace("contrail-analytics/", "src/contrail-analytics/")
-    elif path.startswith("contrail-api-client/"):
-        path = path.replace("contrail-api-client/", "src/contrail-api-client/")
-    elif path.startswith("contrail-controller/"):
-        path = path.replace("contrail-controller/", "controller/")
-    return path
+from xml.etree import ElementTree
 
 
 patchsets = json.load(sys.stdin)
-review_files = set()
-for patchset in patchsets:
-    project = patchset["project"].split('/')[1]
-    review_files.update([project + '/' + file for file in patchset["files"]])
 
 home_dir = os.getenv("HOME", '/root')
-with open("%s/contrail/controller/ci_unittests.json" % home_dir) as fh:
+with open("%s/contrail/controller/ci_unittests.json" % home_dir, 'r') as fh:
     unittests = json.load(fh)
+
+# load vnc structure to evaluate UT targets
+# stdin is a patchsets_info.json file wich has gerrit plain structure
+# ci_unittests.json has vnc structure
+with open("%s/contrail/.repo/manifest.xml" % home_dir, 'r') as f:
+    vnc_raw = ElementTree.parse(f).getroot()
+remotes = dict()
+for remote in vnc_raw.findall(".//remote"):
+    remotes[remote.get('name')] = remote.get('fetch').split('/')[-1]
+projects = dict()
+for project in vnc_raw.findall(".//project"):
+    projects[remotes[project.get('remote')] + '/' + project.get('name')] = project.get('path')
+
+review_files = set()
+for patchset in patchsets:
+    if patchset["project"] not in projects:
+        continue
+    path = projects[patchset["project"]]
+    review_files.update([path + '/' + file for file in patchset["files"]])
 
 actual_targets = set()
 misc_targets = set()
 for ffile in review_files:
-    vnc_file = convert_path(ffile)
     for package in unittests.keys():
         for sd in unittests[package]["source_directories"]:
-            if sd in vnc_file:
+            if sd in ffile:
                 actual_targets.update(unittests[package]["scons_test_targets"])
                 misc_targets.update(unittests[package]["misc_test_targets"])
                 break
