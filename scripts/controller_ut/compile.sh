87474e33354754b7d2c426ea47682ff36836c0ee
Andrey Pavlov <andrey-mp@yandex.ru>
2020-05-07 12:04:03 +0300

    try to switch off full build before UT
    Change-Id: I795796eebc9ee3356b7d8eccfaaa06f2bf798f7c

--- scripts/controller_ut/compile.sh
+++ scripts/controller_ut/compile.sh
@@ -10,4 +10,5 @@ logs_path="/output/logs"
 mkdir -p "$logs_path"
 
 echo "INFO: Run full build $(date)"
-BUILD_ONLY=1 scons -j $JOBS --without-dpdk --kernel-dir=/lib/modules/${KVERS}/build &> $logs_path/build_full.$(date "+%F_%T")
+# BUILD_ONLY=1 scons -j $JOBS --without-dpdk --kernel-dir=/lib/modules/${KVERS}/build &> $logs_path/build_full.$(date "+%F_%T")
+echo "INFO: full build is done $(date)"
