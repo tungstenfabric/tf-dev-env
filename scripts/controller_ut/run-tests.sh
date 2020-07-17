8087a9a1b69791c06365a473f952c09b268337e4
Sergey Baukin <baukin@gmail.com>
2020-07-17 15:33:23 +0300

    skip tests listed at skip_tests file
    Depends-On: I4ed93a3a4677e69e9c66a6f1fe742799453aee74
    Change-Id: I7c744a823d56ca9a57f9500d00012272675a79df

--- scripts/controller_ut/run-tests.sh
+++ scripts/controller_ut/run-tests.sh
@@ -42,7 +42,7 @@ echo ; echo
 for utest in $(cat "$targets_file") ; do
   echo "INFO: $(date) Starting unit tests for target $utest"
   logfilename=$(echo $utest | cut -f 1 -d ':' | rev | cut -f 1 -d '/' | rev)
-  if ! timeout $TARGET_TIMEOUT "$scriptdir/run-tests.py" --less-strict -j $JOBS $utest &> $logs_path/$logfilename ; then
+  if ! timeout $TARGET_TIMEOUT "$scriptdir/run-tests.py" --less-strict -j $JOBS --skip-tests $DEV_ENV_ROOT/skip_tests $utest &> $logs_path/$logfilename ; then
     res=1
     echo "ERROR: $utest failed"
   fi
