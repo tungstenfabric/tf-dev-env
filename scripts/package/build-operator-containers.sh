ebeb89c9474094841565ef339d67059fee2f808c
tikitavi <rtikitavi@gmail.com>
2021-01-26 12:37:03 +0300

    add tf-operator container build to dev-env
    Partial-Jira-bug: CEM-20905
    Signed-off-by: tikitavi <rtikitavi@gmail.com>
    Change-Id: I17767c4ee9510aa65a1f798a6b6730f1aad7c670
    Depends-on: Ifed30588bb0a7c415ef034d42db7f99bf6731023

---
+++ scripts/package/build-operator-containers.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+scriptdir=$(realpath $(dirname "$0"))
+source "$scriptdir/../../common/common.sh"
+source_env
+
+REPODIR=${REPODIR:-"."}
+
+if [[ -z "${CONTRAIL_REGISTRY}" ]]; then
+  echo "CONTRAIL_REGISTRY is not set" && exit 1
+fi
+
+export CONTRAIL_REGISTRY
+
+if [[ -z "${CONTRAIL_REPOSITORY}" ]]; then
+  echo "CONTRAIL_REPOSITORY is not set" && exit 1
+fi
+
+export CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"dev"}
+CONTRAIL_KEEP_LOG_FILES=${CONTRAIL_KEEP_LOG_FILES:-'false'}
+
+function append_log() {
+  local logfile=$1
+  local always_echo=${2:-'false'}
+  while read line ; do
+    if [[ "${CONTRAIL_KEEP_LOG_FILES,,}" != 'true' || "$always_echo" != 'false' ]] ; then
+      echo "$line" | tee -a $logfile
+    else
+      echo "$line" >> $logfile
+    fi
+  done
+}
+
+res=0
+
+operator_logfile="${WORKSPACE}/tf_operator_build_containers.log"
+if [[ -e ${REPODIR}/tf-operator/scripts/build.sh ]]; then
+  ${REPODIR}/tf-operator/scripts/build.sh | append_log $operator_logfile true || res=1
+fi
+
+mkdir -p /output/logs/tf-operator
+# do not fail script if logs file is absent
+mv $operator_logfile /output/logs/tf-operator || /bin/true
+
+exit $res
