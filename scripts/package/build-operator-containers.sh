67edf7a725c6243e2c0bedaa26e76c9d36fff069
alexey-mr <alexey.morlang@gmail.com>
2021-02-05 12:15:31 +0300

    fail build if operator build failed
    Change-Id: Ie2fd7b5935336ebd3bc3fdd9a54784464e65b3b6

--- scripts/package/build-operator-containers.sh
+++ scripts/package/build-operator-containers.sh
@@ -7,13 +7,15 @@ source_env
 REPODIR=${REPODIR:-"."}
 
 if [[ -z "${CONTRAIL_REGISTRY}" ]]; then
-  echo "CONTRAIL_REGISTRY is not set" && exit 1
+  echo "CONTRAIL_REGISTRY is not set"
+  exit 1
 fi
 
 export CONTRAIL_REGISTRY
 
 if [[ -z "${CONTRAIL_REPOSITORY}" ]]; then
-  echo "CONTRAIL_REPOSITORY is not set" && exit 1
+  echo "CONTRAIL_REPOSITORY is not set"
+  exit 1
 fi
 
 export CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"dev"}
@@ -34,12 +36,11 @@ function append_log() {
 res=0
 
 operator_logfile="${WORKSPACE}/tf_operator_build_containers.log"
-if [[ -e ${REPODIR}/tf-operator/scripts/build.sh ]]; then
-  ${REPODIR}/tf-operator/scripts/build.sh | append_log $operator_logfile true || res=1
-fi
+${REPODIR}/tf-operator/scripts/build.sh | append_log $operator_logfile true || res=1
 
 mkdir -p /output/logs/tf-operator
 # do not fail script if logs file is absent
 mv $operator_logfile /output/logs/tf-operator || /bin/true
 
 exit $res
+
