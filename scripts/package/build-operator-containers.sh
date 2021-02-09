c9bd776993d223394ce823ddc5c3a6ea4dcd40df
alexey-mr <alexey.morlang@gmail.com>
2021-02-09 15:28:42 +0300

    Move out scripts from operator
    Change-Id: Id55955d05068da14cf327cb7aeb0e724a05cb9a3

--- scripts/package/build-operator-containers.sh
+++ scripts/package/build-operator-containers.sh
@@ -33,14 +33,38 @@ function append_log() {
   done
 }
 
+function run_cmd(){
+  local me=$(whoami)
+  if [[ "root" == "$me" ]] || ! grep -q "^docker:" /etc/group || groups | grep -q 'docker' ; then
+    $@
+    return
+  fi
+  if ! grep -q "^docker:.*:$me" /etc/group ; then
+    sudo usermod -aG docker $me
+  fi
+  echo $@ | sg docker -c bash
+}
+
+function build_operator() {
+  cd ${REPODIR}/tf-operator
+
+  type go >/dev/null 2>&1 || {
+    export PATH=$PATH:/usr/local/go/bin
+  }
+  export CGO_ENABLED=1
+
+  local target=${CONTAINER_REGISTRY}/tf-operator:${CONTRAIL_CONTAINER_TAG}
+  run_cmd operator-sdk build $target
+  run_cmd docker push $target
+}
+
 res=0
 
 operator_logfile="${WORKSPACE}/tf_operator_build_containers.log"
-${REPODIR}/tf-operator/scripts/build.sh | append_log $operator_logfile true || res=1
+build_operator | append_log $operator_logfile true || res=1
 
 mkdir -p /output/logs/tf-operator
 # do not fail script if logs file is absent
 mv $operator_logfile /output/logs/tf-operator || /bin/true
 
 exit $res
-
