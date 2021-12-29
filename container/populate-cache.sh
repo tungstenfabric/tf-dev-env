b2596702acd164d466f59558656fb749f4946a37
Alexey <alexey.morlang@gmail.com>
2021-12-29 23:44:40 +0300

    added operator-sdk v0.18.2 for cache
    Change-Id: Ie93bfb338e54dace233e275aa7bf2699db5baea4

--- container/populate-cache.sh
+++ container/populate-cache.sh
@@ -13,6 +13,7 @@ pushd $CACHE_DIR
 wget -nv -t3 -P pip/2.7 https://bootstrap.pypa.io/pip/2.7/get-pip.py
 wget -nv -t3 -P go https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
 wget -nv -t3 -P operator-framework/operator-sdk/releases/download/v0.17.2 https://github.com/operator-framework/operator-sdk/releases/download/v0.17.2/operator-sdk-v0.17.2-x86_64-linux-gnu
+wget -nv -t3 -P operator-framework/operator-sdk/releases/download/v0.18.2 https://github.com/operator-framework/operator-sdk/releases/download/v0.18.2/operator-sdk-v0.18.2-x86_64-linux-gnu
 
 wget -nv -t3 -P el8/extras/x86_64 https://pkgs.dyn.su/el8/extras/x86_64/compat-openssl10-devel-1.0.2o-3.el8.x86_64.rpm
 wget -nv -t3 -P pkgs/packages/compat-openssl10/1.0.2o/3.el8/x86_64 https://koji.mbox.centos.org/pkgs/packages/compat-openssl10/1.0.2o/3.el8/x86_64/compat-openssl10-debugsource-1.0.2o-3.el8.x86_64.rpm
