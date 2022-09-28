fc6ff01ceee85d918ca2a647e17fc75f531733a4
Gleb Galkin <gleb108@gmail.com>
2022-09-28 11:45:23 +0300

    Fixed pah for git packages in cache
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: I859b1ce0d90b285478fa3f19eea2a6ff66997441

--- container/populate-cache.sh
+++ container/populate-cache.sh
@@ -20,9 +20,9 @@ wget -nv -t3 -P pkgs/packages/compat-openssl10/1.0.2o/3.el8/x86_64 https://koji.
 
 #Upgrading git in centos7
 #default git 1.18 causes fail (HTTP 402) in fetch-sources job when it's working with gitlab
-wget -nv -t3 -P rhel/7/os/x86_64/git-core-2.37.1-1.ep7.x86_64.rpm https://packages.endpointdev.com/rhel/7/os/x86_64/git-core-2.37.1-1.ep7.x86_64.rpm
-wget -nv -t3 -P rhel/7/os/x86_64/git-2.37.1-1.ep7.x86_64.rpm https://packages.endpointdev.com/rhel/7/os/x86_64/git-2.37.1-1.ep7.x86_64.rpm
-wget -nv -t3 -P rhel/7/os/x86_64/git-core-doc-2.37.1-1.ep7.noarch.rpm https://packages.endpointdev.com/rhel/7/os/x86_64/git-core-doc-2.37.1-1.ep7.noarch.rpm
-wget -nv -t3 -P rhel/7/os/x86_64/perl-Git-2.37.1-1.ep7.noarch.rpm https://packages.endpointdev.com/rhel/7/os/x86_64/perl-Git-2.37.1-1.ep7.noarch.rpm
+wget -nv -t3 -P rhel/7/os/x86_64 https://packages.endpointdev.com/rhel/7/os/x86_64/git-core-2.37.1-1.ep7.x86_64.rpm
+wget -nv -t3 -P rhel/7/os/x86_64 https://packages.endpointdev.com/rhel/7/os/x86_64/git-2.37.1-1.ep7.x86_64.rpm
+wget -nv -t3 -P rhel/7/os/x86_64 https://packages.endpointdev.com/rhel/7/os/x86_64/git-core-doc-2.37.1-1.ep7.noarch.rpm
+wget -nv -t3 -P rhel/7/os/x86_64 https://packages.endpointdev.com/rhel/7/os/x86_64/perl-Git-2.37.1-1.ep7.noarch.rpm
 
 popd
